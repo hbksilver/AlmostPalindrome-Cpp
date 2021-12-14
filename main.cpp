be623d85e56675b2c715198149b6522dd59e1ffe
IlliasBaraka <hassanbaraka@mail.usf.edu>
2021-12-13 17:11:50 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+//#include<bits/stdc++.h>
+using namespace std;
+
+//conversion function
+bool checkPalindrome(string myString){
+
+    int strLength = myString.length();
+    int count = 0;
+    for (int i = 0; i < strLength/2; ++i)
+        if (myString[i] != myString[strLength - i - 1])
+            ++count;
+    return (count <= 1);
+}
+//main function
+int main()
+{
+    string myString;
+    int keepGoing = 0;
+    do {
+        cout << "To continue press 1, to stop program press 0:";
+        cin >> keepGoing;
+        cout << "Enter your string:";
+        cin >> myString;
+        if (checkPalindrome(myString))
+            cout << "The string is almost palindrome." << endl;
+        else
+            cout << "The string is not almost palindrome." << endl;
+        //return 0;
+    }
+    while (keepGoing !=0);
+}
